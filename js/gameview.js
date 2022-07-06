12b5fbe5cacbad95f9c9f990fcb59b6a04e7d241
Chinedum <chinedumike85@gmail.com>
2022-07-06 16:41:50 -0400

    the game board styling

--- js/gameview.js
+++ js/gameview.js
@@ -13,6 +13,17 @@ export default class Gameview{
                     <i class='material-icons'>refresh</i>
                 </button>
             </div>
+            <div class="board">
+                <div class="board__tile" data-index="0">O</div>
+                <div class="board__tile" data-index="1">O</div>
+                <div class="board__tile board__tile--winner" data-index="2">O</div>
+                <div class="board__tile" data-index="3">O</div>
+                <div class="board__tile" data-index="4">O</div>
+                <div class="board__tile" data-index="5">O</div>
+                <div class="board__tile" data-index="6">O</div>
+                <div class="board__tile" data-index="7">O</div>
+                <div class="board__tile" data-index="8">O</div>
+            </div>
         `
     }
 }
